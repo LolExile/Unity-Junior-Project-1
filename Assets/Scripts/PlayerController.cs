7e4cc21fad4c014122dc4422fcc891a0e2cc8465
Johnny Wang <jw072223@gmail.com>
2025-03-28 21:54:30 -0700

    Finished Bonus Content

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public string inputID;
     private float speed = 10.0f;
-    private float turnSpeed = 25.0f;
+    private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float verticalInput;
+    public Camera mainCamera;
+    public Camera altCamera;
+    public KeyCode switchKey;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,9 +20,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        verticalInput = Input.GetAxis("Vertical" + inputID);
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if(Input.GetKeyDown(switchKey)){
+            mainCamera.enabled = !mainCamera.enabled;
+            altCamera.enabled = !altCamera.enabled;
+        }
     }
 }
